70412f34963f01d0d65d805d64e2bacd5a3c5206
Asurvovor <zhangjiliang900123@163.com>
2016-07-10 21:07:02 +0800

    handdle

--- demoToShow/node/server.js
+++ demoToShow/node/server.js
@@ -1,15 +1,17 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;	
 		console.log("Request for " + pathname + " received.");
 
-		route(pathname);
+		//route(handle, pathname);
 
 		response.writeHead(200, {"Content-Type": "text/plain;charset=UTF-8"});
-		response.write("hello, world!长风破浪会有时，直挂云帆济沧海！");
+		var content = route(handle, pathname);
+		response.write(content);
+		//response.write("hello, world!长风破浪会有时，直挂云帆济沧海！");
 
 		response.end();
 	}
