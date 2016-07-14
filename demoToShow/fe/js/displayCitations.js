bceef3ddc568c946480fda2ad8dfd5ef13888cba
unknown <jiliang1@bj-n-203207a.staff.sina.com.cn>
2016-07-14 17:57:00 +0800

    displayCitations

--- demoToShow/fe/js/displayCitations.js
+++ demoToShow/fe/js/displayCitations.js
@@ -1,10 +1,25 @@
 function displayCitations() {
+	if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) return false;
+
+	// 取得所有引用
 	var quotes = document.getElementsByTagName('blockquote');
+
 	for (var i = 0; i < quotes.length; i++) {
-		if (!quotes[i].getAttibute("cite")) continue;
-		var url = quotes[i].getAttibute("cite");
+		if (!quotes[i].getAttribute("cite")) continue;
+		var url = quotes[i].getAttribute("cite");
 		var quoteChildren = quotes[i].getElementsByTagName('*');
 		if (quoteChildren.length < 1) continue;
 		var elem = quoteChildren[quoteChildren.length - 1];
+
+		// 创建标记
+		var link = document.createElement('a');
+		var link_text = document.createTextNode("source");
+		link.appendChild(link_text);
+		link.setAttribute("href", url);
+		var superscript = document.createElement("sup");
+		superscript.appendChild(link);
+		elem.appendChild(superscript);
 	}
 }
+
+addLoadEvent(displayCitations);
