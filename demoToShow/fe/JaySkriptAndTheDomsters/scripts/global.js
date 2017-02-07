5260a18fac4e6fc731290667414da5722228f00b
unknown <jiliang1@bj-n-203207a.staff.sina.com.cn>
2017-02-07 15:27:37 +0800

    live

--- demoToShow/fe/JaySkriptAndTheDomsters/scripts/global.js
+++ demoToShow/fe/JaySkriptAndTheDomsters/scripts/global.js
@@ -218,3 +218,98 @@ function prepareGallery() {
 }
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
+
+//表格
+function addClass(ele, value) {
+  if (!ele.className) {
+    ele.className = value;
+  } else {
+    ele.className += (" " + value);
+  }
+}
+
+function stripeTables() {
+  if (!document.getElementsByTagName) return false;
+  var tables = document.getElementsByTagName("table");
+  for (var i = 0; i < tables.length; i++) {
+    var odd = false;
+    var rows = tables[i].getElementsByTagName("tr");
+    for (var j = 0; j < rows.length; j++) {
+      if (odd == true) {
+        addClass(rows[j], "odd");
+        odd = false;
+      } else {
+        odd = true;
+      }
+    }
+  }
+}
+
+function highlightRows() {
+  if (!document.getElementsByTagName) return false;
+  var rows = document.getElementsByTagName("tr");
+  for (var i = 0; i < rows.length; i++) {
+    rows[i].oldClassName = rows[i].className;
+    rows[i].onmouseover = function() {
+      addClass(this, "highlight");
+    }
+    rows[i].onmouseout = function() {
+      this.className = this.oldClassName;
+    }
+  }
+}
+
+function displayAbbreviations() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.createElement) return false;
+  if (!document.createTextNode) return false;
+
+  //取得所有缩略词
+  var abbreviations = document.getElementsByTagName('abbr');
+  if (abbreviations.length < 1) return false;
+
+  //遍历缩略词
+  var defs = new Array();
+  for (var i = 0; i < abbreviations.length; i++) {
+    var current_abbr = abbreviations[i];
+    if (current_abbr.childNodes.length < 1) continue;
+    var definition = current_abbr.getAttribute("title");
+    var key = current_abbr.lastChild.nodeValue;
+    defs[key] = definition;
+  }
+
+    //创建并遍历定义列表
+  var dlist = document.createElement("dl");
+  for (key in defs) {
+    var definition = defs[key];
+
+    //定义标题
+    var dtitle = document.createElement("dt");
+    var dtitle_text = document.createTextNode(key);
+    dtitle.appendChild(dtitle_text);
+    //定义描述
+    var ddesc = document.createElement("dd");
+    var ddesc_text = document.createTextNode(definition);
+    ddesc.appendChild(ddesc_text);
+    //添加到定义列表
+    dlist.appendChild(dtitle);
+    dlist.appendChild(ddesc);
+  }
+    if (dlist.childNodes.length < 1) return false;
+
+    //定义列表标题
+  var header = document.createElement("h3");
+  var header_text = document.createTextNode("Abbreviations");
+  header.appendChild(header_text);
+
+    //标题和定义列表添加到article
+  var articles = document.getElementsByTagName("article");
+  if (articles.length == 0) return false;
+  var container = articles[0];
+  container.appendChild(header);
+  container.appendChild(dlist);
+}
+
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
+addLoadEvent(displayAbbreviations);
